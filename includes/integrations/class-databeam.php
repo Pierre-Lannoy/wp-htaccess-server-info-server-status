4e8558c325e5412d1435035010024a1801473b5d
Pierre Lannoy <pierre@lannoy.fr>
2020-06-29 09:44:17 +0200

    Full compatibility with PHP 7.4 & Automatic switching between memory and transient when a cache plugin is installed without a properly configured Redis / Memcached.

--- includes/integrations/class-databeam.php
+++ includes/integrations/class-databeam.php
@@ -53,8 +53,8 @@ class Databeam {
 			'version'      => HSISS_VERSION,
 			'subname'      => __( 'KPIs', 'htaccess-server-info-server-status' ),
 			'description'  => __( 'Allows to integrate, as a DataBeam source, all KPIs & indexes collected from the <code>server-status</code> page of this server.', 'htaccess-server-info-server-status' ),
-			'instruction'  => __( 'Just activate-it and use it as source in your favorite visualizers and publishers.', 'htaccess-server-info-server-status' ),
-			'note'         => __( 'Adding this type of data source is reserved to site admins in single site environments and network admins in multisite environments.', 'htaccess-server-info-server-status' ),
+			'instruction'  => __( 'Just add this and use it as source in your favorite visualizers and publishers.', 'htaccess-server-info-server-status' ),
+			'note'         => __( 'In multisite environments, this source is available for all network sites.', 'htaccess-server-info-server-status' ),
 			'legal'        =>
 				[
 					'author'  => 'Pierre Lannoy',
@@ -75,10 +75,9 @@ class Databeam {
 						'method' => 'get_base64_banner',
 					],
 				],
-			'type'         => 'kpi_collection',
-			'roles'        => [ 'network_admin', 'admin' ],
+			'type'         => 'collection::kpi',
 			'restrictions' => [ 'only_network' ],
-			'ttl'          => '15-600',
+			'ttl'          => '15-600:15',
 			'caching'      => [],
 			'data_call'    =>
 				[
@@ -90,46 +89,6 @@ class Databeam {
 			'data_args'    => [],
 		];
 		return $integrations;
-	}
-
-	/**
-	 * Get a raw (SVG) icon.
-	 *
-	 * @param string $name Optional. The name of the file.
-	 * @return  string  The raw value of the SVG file.
-	 * @since   1.0.0
-	 */
-	private static function get_raw( $name = 'banner.svg' ) {
-		$filename = __DIR__ . '/' . $name;
-		if ( ! file_exists( $filename ) ) {
-			$content = '';
-		} else {
-			$content = file_get_contents( $filename );
-		}
-		// phpcs:ignore
-		$id = Cache::id( serialize( [ 'name' => $name, 'squared' => $squared ] ), 'flags/' );
-		if ( Cache::is_memory() ) {
-			$flag = Cache::get_shared( $id );
-			if ( isset( $flag ) ) {
-				return $flag;
-			}
-		} else {
-			if ( array_key_exists( $fname, self::$flags ) ) {
-				return self::$flags[ $fname ];
-			}
-		}
-		if ( ! file_exists( $filename ) ) {
-			return ( 'fr' === $name ? '' : self::get_raw() );
-		}
-		if ( Cache::is_memory() ) {
-			// phpcs:ignore
-			Cache::set_shared( $id, file_get_contents( $filename ), 'infinite' );
-		} else {
-			// phpcs:ignore
-			self::$flags[ $fname ] = file_get_contents( $filename );
-		}
-
-		return ( self::get_raw( $name ) );
 	}
 
 	/**
