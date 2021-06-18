8758f576a009f479f4d6575cd20fadd923b3740d
Pierre Lannoy <pierre@lannoy.fr>
2021-06-18 17:32:44 +0200

    Adds PerfOps One links and branding.

--- admin/class-hsiss-admin.php
+++ admin/class-hsiss-admin.php
@@ -201,7 +201,7 @@ class Hsiss_Admin {
 	public function add_row_meta( $links, $file ) {
 		if ( 0 === strpos( $file, HSISS_SLUG . '/' ) ) {
 			$links[] = '<a href="https://wordpress.org/support/plugin/' . HSISS_SLUG . '/">' . __( 'Support', 'htaccess-server-info-server-status' ) . '</a>';
-			$links[] = '<a href="https://github.com/Pierre-Lannoy/wp-htaccess-server-info-server-status">' . __( 'GitHub repository', 'htaccess-server-info-server-status' ) . '</a>';
+			$links[] = '<a href="https://perfops.one">PefOps One</a>';
 		}
 		return $links;
 	}
