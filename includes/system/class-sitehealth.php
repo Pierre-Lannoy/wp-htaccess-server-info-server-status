3f8472b4930fc10b5f64c1f7fe96fb0089de024e
Pierre Lannoy <pierre@lannoy.fr>
2022-01-06 13:40:21 +0100

    Adapts wording.

--- includes/system/class-sitehealth.php
+++ includes/system/class-sitehealth.php
@@ -120,7 +120,7 @@ class Sitehealth {
 		$key = 'perfopsone_objectcache';
 		if ( ! array_key_exists( $key, $debug_info ) ) {
 			$debug_info[ $key ] = [
-				'label'  => esc_html__( 'Object cache', 'htaccess-server-info-server-status' ),
+				'label'  => 'PerfOps One - ' . esc_html__( 'Object cache', 'htaccess-server-info-server-status' ),
 				'fields' => Cache::debug_info(),
 			];
 		}
