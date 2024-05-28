22c344469ec371f40c6e526f91cf02bf80f2fe02
Pierre Lannoy <pierre@hosterra.eu>
2024-05-28 14:04:06 +0200

    Ready to release 3.0.0 on wp.org

--- htaccess-server-info-server-status.php
+++ htaccess-server-info-server-status.php
@@ -10,7 +10,7 @@
  * Plugin Name:       Apache Status & Info
  * Plugin URI:        https://perfops.one/htaccess-server-info-server-status
  * Description:       Apache server-info and server-status monitoring right in your WordPress admin.
- * Version:           2.15.0
+ * Version:           3.0.0
  * Requires at least: 6.2
  * Requires PHP:      8.1
  * Author:            Pierre Lannoy / PerfOps One
