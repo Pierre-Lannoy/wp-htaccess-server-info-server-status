a170a3528d656f3f7e911f6637ce2d5f6d6bd3ec
Pierre Lannoy <pierre@hosterra.eu>
2024-11-22 10:03:10 +0100

    Uses object cache first, then transient…

--- includes/plugin/class-updater.php
+++ includes/plugin/class-updater.php
@@ -12,7 +12,7 @@ namespace Hsiss\Plugin;
 use Hsiss\System\Nag;
 use Hsiss\System\Option;
 use Hsiss\System\Environment;
-
+use Hsiss\System\Cache;
 use Hsiss\System\Role;
 use Exception;
 use Hsiss\System\Markdown;
@@ -107,7 +107,7 @@ class Updater {
 	 * @return  object   The remote info.
 	 */
 	private function gather_info() {
-		$remotes = get_transient( 'update-' . $this->slug );
+		$remotes = Cache::get_global( 'data_update-infos' );
 		if ( ! $remotes ) {
 			$remotes = new \stdClass();
 
@@ -131,7 +131,7 @@ class Updater {
 			$remotes->author_profile = $plugin_info['author_profile'] ?? 'https://profiles.wordpress.org/pierrelannoy/';
 
 			$remote = wp_remote_get(
-				str_replace( 'github.com', 'api.github.com/repos', $this->product ) . '/releases/latest',
+				'https://releases.perfops.one/' . $this->slug . '.json',
 				[
 					'timeout' => 10,
 					'headers' => [
@@ -155,7 +155,7 @@ class Updater {
 				return false;
 			}
 
-			set_transient( 'update-' . $this->slug, $remotes, DAY_IN_SECONDS );
+			Cache::set_global( 'data_update-infos', $remotes, DAY_IN_SECONDS );
 		}
 
 		return $remotes;
